22efc69259ad00fd4321944404d2202176c81757
manuel3g <manuel3g@users.noreply.github.com>
2018-10-08 21:30:04 -0600

    Initial commit

---
+++ app/src/main/java/m3g/com/nuevo/Principal.java
@@ -0,0 +1,13 @@
+package m3g.com.nuevo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
