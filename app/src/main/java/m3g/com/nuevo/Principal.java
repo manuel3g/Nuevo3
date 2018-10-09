23a42162b289e24fd48048dbec85d95e936b719c
Nelson Cortez <nelsoncortez77@gmail.com>
2018-10-08 22:37:41 -0600

    jhjb

--- app/src/main/java/m3g/com/nuevo/Principal.java
+++ app/src/main/java/m3g/com/nuevo/Principal.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Principal extends AppCompatActivity {
-    private String prueba;
+    private String pruebawn,nk,j;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
