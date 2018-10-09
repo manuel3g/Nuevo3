19fcf1d44517e65a8c379dd02010d82efd92f6d6
Nelson Cortez <nelsoncortez77@gmail.com>
2018-10-08 22:31:34 -0600

    Prueba

--- app/src/main/java/m3g/com/nuevo/Principal.java
+++ app/src/main/java/m3g/com/nuevo/Principal.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Principal extends AppCompatActivity {
-    private String Prueba;
+    private String Prueba2;
 
 
     @Override
